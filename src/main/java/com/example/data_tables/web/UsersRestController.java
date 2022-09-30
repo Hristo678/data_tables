7d3db3a8805059775c077bf8d8b1af8258162996
Hristo <fenimo@abv.bg>
2022-09-30 11:54:21 +0300

    New In Test

--- src/main/java/com/example/data_tables/web/UsersRestController.java
+++ src/main/java/com/example/data_tables/web/UsersRestController.java
@@ -34,6 +34,7 @@ public class UsersRestController {
 
     String newInDevelop = "newInDevelop";
     String newInDevelop2 = "newInDevelop2";
+    String test2 = "test2";
 //    @PostMapping("/users/add")
 //    public String addUser(@RequestParam(value = "selected") String selected){
 //        System.out.println();
